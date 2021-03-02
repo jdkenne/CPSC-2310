f4b235a4e016d50559d97bf2774abfbd75d7090f
jdkenne <jdkenne@g.clemson.edu>
2021-03-01 19:37:30 -0500

    Added Lab6 from the course

---
+++ Lab6/driver.c
@@ -0,0 +1,19 @@
+#include "src/functions.h"
+
+
+int main(int argc, char* argv[])
+{
+    FILE* input = fopen("input.ppm", "r");
+    FILE* output = fopen("output.ppm", "w");
+
+    header_t hdr;
+    readHeader(&hdr, input);
+    pixel_t* pix = (pixel_t*) malloc (hdr.width*hdr.height*sizeof(pixel_t));;
+    readPixls(pix, input, hdr);
+    color2Negative(hdr, pix);
+    writeImage(pix, hdr, output);
+
+   free(pix);
+   return 0;
+
+}
